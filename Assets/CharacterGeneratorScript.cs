eee09cf892f50a982435bb6b3f7352a20fc1b68d
philt5252 <philt5252@gmail.com>
2014-08-26 20:08:09 -0500

    heretic punching time!

---
+++ Assets/CharacterGeneratorScript.cs
@@ -0,0 +1,60 @@
+﻿
+using System.Diagnostics;
+using System.Security.Principal;
+using System.Timers;
+using UnityEngine;
+using System.Collections;
+
+public class CharacterGeneratorScript : MonoBehaviour
+{
+
+    public GameObject[] Characters;
+    public float MinTime = 1f;
+    public float MaxTime = 3f;
+
+    private float currentTimeSpan;
+
+    private Stopwatch stopwatch = new Stopwatch();
+    private GameObject nextCharacter;
+    // Use this for initialization
+    private void Start()
+    {
+        stopwatch.Start();
+        UpdateAll();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (stopwatch.ElapsedMilliseconds > currentTimeSpan*1000)
+        {
+            stopwatch.Reset();
+            stopwatch.Start();
+
+            Instantiate(nextCharacter, transform.position, Quaternion.identity);
+
+            UpdateAll();
+        }
+    }
+
+    private void UpdateCurrentTimeSpan()
+    {
+        currentTimeSpan = Random.Range(MinTime, MaxTime);
+    }
+
+    private void UpdateNextCharacter()
+    {
+        nextCharacter = Characters[Random.Range(0, Characters.Length)];
+    }
+
+    private void UpdateAll()
+    {
+        UpdateCurrentTimeSpan();
+        UpdateNextCharacter();
+    }
+}
