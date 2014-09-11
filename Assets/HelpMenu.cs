7091856463ee7e1b02a982cac85dea05694ef1f3
philt5252 <philt5252@gmail.com>
2014-09-11 18:19:14 -0500

    Menus and starting timer

---
+++ Assets/HelpMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.CodeDom;
+using UnityEngine;
+using System.Collections;
+
+public class HelpMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(Screen.width-55, Screen.height-55, 50, 50), "Back"))
+        {
+            
+            Application.LoadLevel("MainMenu");
+            //Application.LoadLevelAsync("Start");
+        }
+    }
+}
---
+++ Assets/TimerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class TimerController : MonoBehaviour
+{
+    public TimeSpan TimeRemaining;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
